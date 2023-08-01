6fe777c831584f79e3cd5f95e449626f59862a5c
Katharine-Walters <kwalte@umich.edu>
2023-08-01 12:15:19 -0400

    working except for negative belt speed

--- BertecMan.py
+++ BertecMan.py
@@ -2,41 +2,76 @@ import socket
 from random import randint
 import time
 from SoftRealtimeLoop import SoftRealtimeLoop
+from threading import Thread
+import struct
+import numpy as np
 
 class Bertec:
     """
     A class for reading Bertec speed and incline over the network 
-    Katharine Walters 07/23
+    Katharine Walters 08/23
     """
-    def __init__(self, viconPC_IP = '192.168.1.134', viconPC_BertecPort = 4000):
-
+    def __init__(self, viconPC_IP = '141.212.77.30', viconPC_BertecPort = 4000):
         self.destinationIP = viconPC_IP
         self.destinationPort = viconPC_BertecPort
 
         # Setup TCP
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        self.sock.bind((self.destinationIP, self.destinationPort))
-        self.sock.listen()
-        self.conn, self.addr = self.sock.accept()
+        self.sock.connect((self.destinationIP, self.destinationPort))
 
-    def __exit__(self):
+        self.thread = Thread(target=self._update, args = ())
+        self.stopped = False
+
+        self.belt_speed = [0,0]
+        self.incline = 0.0
+
+    def start(self):
+        self.thread.start()
+        return self
+    
+    def stop(self):
+        self.stopped = True
+
+    def __del__(self):
+        self.thread.join()
         self.sock.close()
+        print("Bertec closed")
+
+    def _update(self):
+        while True:
+            if self.stopped:
+                break
+            packet = self.sock.recv(32)
+            data = list(packet)
+            belt_speedR = ((data[1]<<8) + data[2])/1000
+            belt_speedL = ((data[3]<<8) + data[4])/1000
+            self.belt_speed = [belt_speedL, belt_speedR]
+            self.incline = ((data[9]<<8) + data[10])/100
+            
+
+    def get_treadmill_incline(self):
+        # Read treadmill incline 
+        return self.incline
 
     def get_belt_speed(self):
-        return self.conn.recv(1024)
+        # Read belt speed 
+        return self.belt_speed
 
 if __name__ == '__main__':
     bertec = Bertec()
+    bertec.start()
 
-    loop = SoftRealtimeLoop(dt = 1/200, report=True, fade=0.01)
-    t0 = time.time()
     i = 0
+    loop = SoftRealtimeLoop(dt = 1/100, report=True, fade=0.01)
+    t0 = time.time() 
     for t in loop: 
-        i = i+1
-
+        i = i + 1
         t_curr = time.time() - t0
-        speed = bertec.get_belt_speed()
+        speedL, speedR = bertec.get_belt_speed()
+        incline = bertec.get_treadmill_incline()
 
-        if i >= 50:
+        if i >=10:
             i = 0
-            print("time ", t_curr)
+            print("time ", t_curr, " speed ", speedL, " ", speedR, " incline ", incline)
+    
+    bertec.stop()
