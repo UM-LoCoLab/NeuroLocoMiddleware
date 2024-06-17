3bf461cf97413e0d8dd8d4738732f835fffec51f
Jed Simms <jedsimms7@tamu.edu>
2024-06-17 20:12:44 +0100

    Commented out a print statement in udp_synchA

--- src/NeuroLocoMiddleware/test/test_udp_synchA.py
+++ src/NeuroLocoMiddleware/test/test_udp_synchA.py
@@ -1,20 +1,20 @@
 from FindLibrariesWarning import *
-from SoftRealtimeLoop import SoftRealtimeLoop
-from UdpBinarySynch import UdpBinarySynchA
+from NeuroLocoMiddleware.SoftRealtimeLoop import SoftRealtimeLoop
+import UdpBinarySynch as ubs
 import numpy as np
 
 
 
-print("A1")
+# print("A1")
 # context = zmq.Context()
 # print("A2")
 
 
-synch = UdpBinarySynchA(
-    recv_IP="127.0.0.1",
-    recv_port=5557,
-    send_IP="127.0.0.1", 
-    send_port=5558)
+synch = ubs.UdpBinarySynchA(
+    recv_IP="192.168.1.157",
+    recv_port=5558,
+    send_IP="192.168.1.216", 
+    send_port=5557)
 
-for t in SoftRealtimeLoop(0.001, report=True):
-    print(synch.update(np.array([42.1, t*1000])))
+for t in SoftRealtimeLoop(0.000001, report=True):
+    print(synch.update(np.array([])))
